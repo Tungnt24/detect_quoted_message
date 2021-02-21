6327818ed7f1160d126c5a6e8cd8aed25c83c663
Tungnt24 <nguyentutung24@gmail.com>
2021-02-21 23:34:54 +0700

    update logic

--- example_data.py
+++ example_data.py
@@ -1,13 +1,21 @@
 FIRST_DATA = """
-okok
-On 2/18/21 8:46 AM, Tungnt wrote:
-> ok thaasy r nhes
-> 
-> On 2/18/21 8:46 AM, Tungnt24 wrote:
->> đây là test plain texty
+Tin nhắn 4
+
+On 2/20/21 11:16 PM, tungnt24 wrote:
+> Tin nhắn 3
+>
+> Vào CN, 20 thg 2, 2021 vào lúc 23:15 Tungnt <tungnguyentu@bizflycloud.vn>
+> đã viết:
+>
+>> Tin nhắn 2
+>>
+>> On 2/20/21 11:15 PM, tungnt24 wrote:
+>>> Tin nhắn 1
+>>>
+>>
+>
 """
 
 SECOND_DATA = """
-<p>second message</p>\n<p>&nbsp;</p>\n<p>V&agrave;o 15:03, 28/01/2021,<strong>B</strong> &lt;<a href=\"mailto:nameB@gmail.com\" target=\"_blank\"rel=\"noopener\">nameB@gmail.com</a>&gt; đ&atilde; viết:</p>\n<blockquote>\n<div     
-dir=\"ltr\">first message</div></blockquote>
+<p>Tin nhắn 4</p><p>&nbsp;</p><p>V&agrave;o 23:23, 20/02/2021, <strong>tungnt24</strong> &lt;nguyentutung24@gmail.com&gt; đ&atilde; viết:</p><blockquote><div dir="ltr">Tin nh&aacute;n 3</div><br /><div class="gmail_quote"><div class="gmail_attr" dir="ltr">V&agrave;o CN, 20 thg 2, 2021 vào lúc 23:23 Tungnt &lt;<a href="mailto:tungnguyentu@bizflycloud.vn">tungnguyentu@bizflycloud.vn</a>&gt; đ&atilde; viết:</div><blockquote class="gmail_quote" style="margin: 0px 0px 0px 0.8ex; border-left: 1px solid #cccccc; padding-left: 1ex;">Tin nhắn 2<br /><br /></blockquote></div></blockquote>
 """
